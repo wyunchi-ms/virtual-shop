2f75846940675a560c1cef26c0086ab36e2fe1e3
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2022-08-12 08:26:37 +0800

    Add yaml

---
+++ supermarket/Assets/Scripts/ConfigManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using YamlDotNet.Serialization;
+using UnityEngine;
+using System.IO;
+
+public class ConfigManager : MonoBehaviour
+{
+    public GameConfig gameConfig;
+    
+    void Start()
+    {
+        var deserializer = new DeserializerBuilder()
+            .Build();
+        string content = File.ReadAllText(@"E:\project\virtual-shop\supermarket\Configs\Game.yml");
+        gameConfig = deserializer.Deserialize<GameConfig>(content);
+    }
+}
